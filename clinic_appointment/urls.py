cc6caadce218cceba58abe47fd3354fa28bd4f1f
Mustafa Tarek <mustafatarek104@gmail.com>
2026-02-27 01:20:29 +0200

    Applying mvt pattern

--- clinic_appointment/urls.py
+++ clinic_appointment/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', include('dashboard.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('appointments/', include('appointments.urls')),
+    path('scheduling/', include('scheduling.urls')),
+    path('records/', include('medical_records.urls')),
 ]
