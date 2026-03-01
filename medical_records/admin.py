72a2f00758b87c34057a0f5daf1f444347efdad8
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-03-01 04:51:01 +0200

    added admin configurations for CRUD to the consulations

--- medical_records/admin.py
+++ medical_records/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ConsultationRecord
 # Register your models here.
+
+@admin.register(ConsultationRecord)
+class ConsultationRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'appointment', 'diagnosis', 'created_at')
+    search_fields = ('diagnosis', 'notes', 'prescription', 'requested_tests')
+    list_filter = ('created_at',)
