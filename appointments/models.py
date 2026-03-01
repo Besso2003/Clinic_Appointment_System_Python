1e535f1249180bf1c9f647890b782ce6e72ffee2
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-03-01 04:39:27 +0200

    added ConsulationRecord model and configured the database along with migrations

--- appointments/models.py
+++ appointments/models.py
@@ -1,51 +1,51 @@
-# Would uncomment it after finishing doc , patient and slots models
+#Would uncomment it after finishing doc , patient and slots models
 
-# from django.db import models
+from django.db import models
 
-# class Appointment(models.Model):
-#     STATUS_CHOICES = [
-#         ("REQUESTED", "Requested"),
-#         ("CONFIRMED", "Confirmed"),
-#         ("CHECKED_IN", "Checked In"),
-#         ("COMPLETED", "Completed"),
-#         ("CANCELLED", "Cancelled"),
-#         ("NO_SHOW", "No Show"),
-#     ]
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ("REQUESTED", "Requested"),
+        ("CONFIRMED", "Confirmed"),
+        ("CHECKED_IN", "Checked In"),
+        ("COMPLETED", "Completed"),
+        ("CANCELLED", "Cancelled"),
+        ("NO_SHOW", "No Show"),
+    ]
 
-#     patient = models.ForeignKey(
-#         "User",  
-#         on_delete=models.CASCADE,
-#         related_name="appointments_as_patient"
-#     )
+    # patient = models.ForeignKey(
+    #     "User",  
+    #     on_delete=models.CASCADE,
+    #     related_name="appointments_as_patient"
+    # )
 
-#     doctor = models.ForeignKey(
-#         "User",  
-#         on_delete=models.CASCADE,
-#         related_name="appointments_as_doctor"
-#     )
+    # doctor = models.ForeignKey(
+    #     "User",  
+    #     on_delete=models.CASCADE,
+    #     related_name="appointments_as_doctor"
+    # )
 
-#     slot = models.ForeignKey(
-#         "Slot", 
-#         on_delete=models.CASCADE,
-#         related_name="appointments"
-#     )
+    # slot = models.ForeignKey(
+    #     "Slot", 
+    #     on_delete=models.CASCADE,
+    #     related_name="appointments"
+    # )
 
-#     status = models.CharField(
-#         max_length=20,
-#         choices=STATUS_CHOICES,
-#         default="REQUESTED"
-#     )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="REQUESTED"
+    )
 
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     class Meta:
-#         constraints = [
-#             models.UniqueConstraint(
-#                 fields=["slot"],
-#                 name="unique_slot_booking"
-#             )
-#         ]
+    # class Meta:
+    #     constraints = [
+    #         models.UniqueConstraint(
+    #             fields=["slot"],
+    #             name="unique_slot_booking"
+    #         )
+    #     ]
 
-#     def __str__(self):
-#         return f"{self.patient.username} → {self.doctor.username} | {self.status}"
+    def __str__(self):
+        return f"{self.patient.username} → {self.doctor.username} | {self.status}"
