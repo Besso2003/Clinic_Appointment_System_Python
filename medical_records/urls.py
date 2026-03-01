54a89a04577114d081db0486ca714f16bd481ae7
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-03-01 13:44:38 +0200

    creation view and template added but not tested

--- medical_records/urls.py
+++ medical_records/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from .views import CreateConsultationRecord
 
 urlpatterns = [
+    path(
+        'create/<int:appointment_id>',
+        CreateConsultationRecord.as_view(),
+        name='consultation-create'
+    ),
 ]
--- medical_records/views.py
+++ medical_records/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from .models import ConsultationRecord
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from appointments.models import Appointment
 # Create your views here.
+
+class CreateConsultationRecord(CreateView):
+    model = ConsultationRecord
+    template_name = 'medical_records/consultationrecord_form.html'
+    fields = ['diagnosis', 'notes', 'prescription', 'requested_tests']
+    success_url = reverse_lazy("consultation-list")
+
+def form_valid(self, form):
+    appointment = get_object_or_404(
+        Appointment,
+        id=self.kwargs['appointment_id']
+    )
+    form.instance.appointment = appointment
+    return super().form_valid(form)
