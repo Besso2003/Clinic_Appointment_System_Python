efcf23b982023d5f2d0508a7f42248f5445005a5
Mustafa Tarek <mustafatarek104@gmail.com>
2026-03-03 07:49:59 +0200

    Adding Admin Features to Admin Dashboard

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,13 +5,12 @@ from django.contrib.auth.models import Group
 
 User = get_user_model()
 
-class UserRegistrationForm(UserCreationForm):
+class StaffRegistrationForm(UserCreationForm):
     ALLOWED_ROLES = [
-        ('P', 'Patient'),
+        ('A', 'Admin'),
         ('D', 'Doctor'),
         ('R', 'Receptionist'),
     ]
-    
     role = forms.ChoiceField(choices=ALLOWED_ROLES, required=True)
 
     class Meta(UserCreationForm.Meta):
@@ -21,15 +20,26 @@ class UserRegistrationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         role = self.cleaned_data.get('role')
+        user.is_staff = True 
         if commit:
             user.save()
-            if role:
-                group_name = self._role_to_group(role)
-                if group_name:
-                    group, _ = Group.objects.get_or_create(name=group_name)
-                    user.groups.add(group)
+            group_name = {'D': 'Doctor', 'R': 'Receptionist', 'A': 'Admin'}.get(role)
+            if group_name:
+                group, _ = Group.objects.get_or_create(name=group_name)
+                user.groups.add(group)
         return user
 
-    def _role_to_group(self, code):
-        mapping = {'P': 'Patient', 'D': 'Doctor', 'R': 'Receptionist'} # 'A' removed
-        return mapping.get(code)
+
+class PatientRegistrationForm(UserCreationForm):    
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "first_name", "last_name", "email", "profile_picture")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'P' 
+        if commit:
+            user.save()
+            group, _ = Group.objects.get_or_create(name='Patient')
+            user.groups.add(group)
+        return user
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('register/', views.register_view, name='register'),
+    path('register/', views.patient_register_view, name='register'),
+    path('register/staff/', views.staff_register_view, name='staff_register'),
     path('login/', views.login_view.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/', views.view_profile, name='profile')
+    path('profile/', views.view_profile, name='profile'),
+    path('staff/', views.manage_staff_view, name='manage_staff'),
+    path('staff/edit/<int:staff_id>/', views.view_staff_profile, name='edit_staff'),
+    path('staff/delete/<int:staff_id>/', views.delete_staff, name='delete_staff'),
 ]
