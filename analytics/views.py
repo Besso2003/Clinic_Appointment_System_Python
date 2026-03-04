9a157b0c745887f02d073f0e5b214186e2e0b62f
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-03-04 19:44:46 +0200

    modified analytics template and view to enusre same style and handling admin check

--- analytics/views.py
+++ analytics/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import TemplateView
-from django.db.models import Count, Q
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Count
+from django.db.models.functions import ExtractHour
 from appointments.models import Appointment
 from scheduling.models import Slot
-from medical_records.models import ConsultationRecord
-from django.db.models.functions import ExtractHour
-from accounts.models import User
 
-class AdminAnalytics(TemplateView):
+
+class AdminAnalytics(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     template_name = "analytics/admin_analytics.html"
+
+    # Only allow admins
+    def test_func(self):
+        return self.request.user.role == 'A'
+
+    def handle_no_permission(self):
+        from django.shortcuts import redirect
+        return redirect('home')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         total_appointments = Appointment.objects.count()
-
         completed = Appointment.objects.filter(status="COMPLETED").count()
         no_show = Appointment.objects.filter(status="NO_SHOW").count()
 
@@ -27,15 +31,6 @@ class AdminAnalytics(TemplateView):
         booked_slots = Slot.objects.filter(is_available=False).count()
         utilization_rate = (booked_slots / total_slots * 100) if total_slots else 0
 
-        context.update({
-            "total_appointments": total_appointments,
-            "completion_rate": round(completion_rate, 1),
-            "no_show_rate": round(no_show_rate, 1),
-            "utilization_rate": round(utilization_rate, 1),
-        })
-    
-        ### Peak Hours
-
         peak_hours = (
             Appointment.objects
             .annotate(hour=ExtractHour("slot__start_time"))
@@ -44,12 +39,6 @@ class AdminAnalytics(TemplateView):
             .order_by("-total")
         )
 
-        context["peak_hours"] = peak_hours
-
-
-        ### Doctors performance
-
-
         doctor_stats = (
             Appointment.objects
             .values("doctor__first_name", "doctor__last_name")
@@ -57,6 +46,13 @@ class AdminAnalytics(TemplateView):
             .order_by("-total")
         )
 
-        context["doctor_stats"] = doctor_stats
+        context.update({
+            "total_appointments": total_appointments,
+            "completion_rate": round(completion_rate, 1),
+            "no_show_rate": round(no_show_rate, 1),
+            "utilization_rate": round(utilization_rate, 1),
+            "peak_hours": peak_hours,
+            "doctor_stats": doctor_stats,
+        })
 
         return context
